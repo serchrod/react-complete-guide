881d8dd29ecda8ab682fd1e064e5db1ddcf44466
serchrod <dannyrodr1996@gmail.com>
2021-04-24 10:47:17 -0600

    Creating new component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Learrning Rect
-        </p>
-        <Person></Person>
-      </header>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
