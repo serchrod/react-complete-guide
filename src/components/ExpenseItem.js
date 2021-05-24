45751175d8d29336f136f8320348bdaf0f21dd40
serchrod <dannyrodr1996@gmail.com>
2021-05-23 21:41:29 -0600

    Second Lesson

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
-function ExpenseItem(){
+function ExpenseItem(props){
+
+    const expenseTitle = props.data?.title;
+    const expenseAmount = props.data?.amount;
+
     return  <div className="expense-item">
-        <div>March 28th 2021</div>
+         <ExpenseDate date={props.data?.date}></ExpenseDate>    
         <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$294.71</div>
+            <h2>{expenseTitle}</h2> 
+            <div className="expense-item__price">${expenseAmount}</div>
         </div>
     </div>
 }
 
-export default ExpenseItem;
+export default ExpenseItem; 
